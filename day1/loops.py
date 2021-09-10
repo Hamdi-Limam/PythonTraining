1cb40bbfe34b011449fb21bd1eaa4137c98b27b6
Hamdi-Limam <limamhamdi13@gmail.com>
2021-09-10 17:47:06 +0100

    Added New file -loops for day1 task2

---
+++ day1/loops.py
@@ -0,0 +1,41 @@
+# for loop string
+for x in "Python":
+    print(x, end=" ")
+else:
+    print(" ")
+    print("Finally finished!")
+
+# for loop numbers
+print("Printing range(5) loop:")
+for x in range(5):
+    print(x, end=" ")
+
+print(" ")
+print("Printing range(2,5) loop:")
+for x in range(2,5):
+    print(x, end=" ")
+
+print(" ")
+print("Printing range(0,10,2) loop:")
+for x in range(0,10,2):
+    print(x, end=" ")
+
+# for..else
+print(" ")
+print("Checking if an array has a name that starts with A:", end=" ")
+names = ["Hamdi", "Achref"]
+for name in names:
+    if name.startswith("A"):
+        print("Found")
+        break
+else:
+    print("name not found")
+
+# while..else loops
+guess = 0
+answer = 5
+
+while answer != guess:
+    guess = int(input("Guess: "))
+else:
+    print("Congratz you guessed the answer", type(guess))
