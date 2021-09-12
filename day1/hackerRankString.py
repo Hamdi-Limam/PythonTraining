2f4bb07dd7bfa53df903bb9fedb73ce7856e3664
hnaitlimam <limamhmadi13@gmail.com>
2021-09-12 10:30:10 +0100

    Added branch day 2

--- day1/hackerRankString.py
+++
@@ -1,8 +0,0 @@
-def split_and_join(line):
-    line = line.split(" ")
-    return "-".join(line)
-
-if __name__ == '__main__':
-    line = input("Enter a string: ")
-    result = split_and_join(line)
-    print(result)
