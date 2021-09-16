c736dd8b6407a0da9c6ed212c0ce3f6b4e8a0ed6
hnaitlimam <limamhmadi13@gmail.com>
2021-09-16 20:26:00 +0100

    Added a shorter solution in the file -hackerRankValidateCreditCard for day3

--- day3/hacker_rank_problems/hackerRankValidateCreditCard.py
+++ day3/hacker_rank_problems/hackerRankValidateCreditCard.py
@@ -11,3 +11,8 @@ digits_pattern = re.compile(r"^[456]\d{15}$|^[456](\d{3}-)(\d{4}-){2}\d{4}$")
 for i in lines:
     verify_repeat = re.search(r"(\d)-*?\1-*?\1-*?\1", i, re.I)
     print("Valid" if verify_repeat == None and digits_pattern.search(i) else "Invalid")
+
+# Shorter solution
+# TESTER = re.compile(r"^"r"(?!.*(\d)(-?\1){3})"r"[456]"r"\d{3}"r"(?:-?\d{4}){3}"r"$")
+# for _ in range(int(input().strip())):
+#     print("Valid" if TESTER.search(input().strip()) else "Invalid")
