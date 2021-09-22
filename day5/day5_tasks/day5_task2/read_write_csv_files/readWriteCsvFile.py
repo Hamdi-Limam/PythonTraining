8b8b123216cbd90104da85e62a54124be6b673ca
Hamdi-Limam <limamhamdi13@gmail.com>
2021-09-22 09:22:42 +0100

    Added read_write_csv_files of day5 task2

---
+++ day5/day5_tasks/day5_task2/read_write_csv_files/readWriteCsvFile.py
@@ -0,0 +1,40 @@
+import csv
+
+with open('employee_birthday.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
+
+
+# Here’s the code to read it in as a dictionary this time:
+with open('employee_birthday.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        print(f'\t{row["name"]} works in the {row["department"]} department, and was born in {row["birthday month"]}.')
+        line_count += 1
+    print(f'Processed {line_count} lines.')
+
+# Writing CSV Files With csv
+with open('employee_file.csv', mode='w') as employee_file:
+    employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    employee_writer.writerow(['John Smith', 'Accounting', 'November'])
+    employee_writer.writerow(['Erica Meyers', 'IT', 'March'])
+
+with open('employee_file2.csv', mode='w') as csv_file:
+    fieldnames = ['emp_name', 'dept', 'birth_month']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerow({'emp_name': 'John Smith', 'dept': 'Accounting', 'birth_month': 'November'})
+    writer.writerow({'emp_name': 'Erica Meyers', 'dept': 'IT', 'birth_month': 'March'})
